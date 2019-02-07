487ab4824b8b4c0372d026b53f85d524c4652488
hedgar2017 <hedgar2017@gmail.com>
2019-02-07 13:54:29 +0200

    Comment unfinished code

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 
 use std::{io, fs, num, collections::VecDeque};
 
+use log::*;
+
 use patch_rs::{Patch, PatchError, PatchProcessor};
 
 #[derive(Debug)]
@@ -62,51 +64,67 @@ fn main() -> InterdiffResult {
     let context_radius = args.value_of("context_radius").expect("Unreachable");
     let _context_radius: usize = context_radius.parse().map_err(Error::ContextRadius)?;
 
-    let mut interdiff = Patch {
-        input: patch_1.output.to_owned(),
-        output: patch_2.output.to_owned(),
-        contexts: VecDeque::new(),
-    };
-    let mut patch_1_offset = 0;
-    let mut patch_2_offset = 0;
+//    let c1 = patch_1.contexts.front().unwrap();
+//    let c2 = patch_2.contexts.front().unwrap();
+//    let sum = c1.flip().pop_front().unwrap().merge(c2.reduce().pop_front().unwrap());
+//    trace!("\n{}", sum);
 
-    while !patch_1.contexts.is_empty() && !patch_2.contexts.is_empty() {
-        let p1 = patch_1.contexts.front().unwrap();
-        let p2 = patch_2.contexts.front().unwrap();
-        if p1.header.file1_l > p2.header.file1_l {
-            let reduced = patch_2.contexts.pop_front().unwrap().reduce();
-            for mut context in reduced.into_iter() {
-                patch_2_offset += context.offset();
-                context.shift(patch_1_offset);
-                interdiff.contexts.push_back(context);
-            }
-        } else {
-            let flipped = patch_1.contexts.pop_front().unwrap().flip();
-            for mut context in flipped.into_iter() {
-                patch_1_offset += context.offset();
-                context.shift(patch_2_offset);
-                interdiff.contexts.push_back(context);
-            }
-        }
-    }
-    while !patch_1.contexts.is_empty() {
-        let flipped = patch_1.contexts.pop_front().unwrap().flip();
-        for mut context in flipped.into_iter() {
-            patch_1_offset += context.offset();
-            context.shift(patch_2_offset);
-            interdiff.contexts.push_back(context);
-        }
-    }
-    while !patch_2.contexts.is_empty() {
-        let reduced = patch_2.contexts.pop_front().unwrap().reduce();
-        for mut context in reduced.into_iter() {
-            patch_2_offset += context.offset();
-            context.shift(patch_1_offset);
-            interdiff.contexts.push_back(context);
-        }
-    }
-
-    println!("{}", interdiff);
+//    let mut interdiff = Patch {
+//        input: patch_1.output.to_owned(),
+//        output: patch_2.output.to_owned(),
+//        contexts: VecDeque::new(),
+//    };
+//    let mut patch_1_offset = 0;
+//    let mut patch_2_offset = 0;
+//
+//    trace!("DRAINING BOTH PATCHES");
+//    while !patch_1.contexts.is_empty() && !patch_2.contexts.is_empty() {
+//        let p1 = patch_1.contexts.front().unwrap();
+//        let p2 = patch_2.contexts.front().unwrap();
+//        if p1.header.file1_l + (patch_1_offset as usize) <= p2.header.file1_l + (patch_2_offset as usize) {
+//            let flipped = patch_1.contexts.pop_front().unwrap().flip();
+//            for mut context in flipped.into_iter() {
+////                patch_1_offset += context.offset();
+////                trace!("PATCH 1 OFFSET: {}", patch_1_offset);
+////                context.shift(patch_2_offset);
+////                trace!("CONTEXT 1: {}", context);
+//                interdiff.contexts.push_back(context);
+//            }
+//        } else {
+//            let reduced = patch_2.contexts.pop_front().unwrap().reduce();
+//            for mut context in reduced.into_iter() {
+////                patch_2_offset += context.offset();
+////                trace!("PATCH 2 OFFSET: {}", patch_2_offset);
+////                context.shift(patch_1_offset);
+////                trace!("CONTEXT 2: {}", context);
+//                interdiff.contexts.push_back(context);
+//            }
+//        }
+//    }
+//    trace!("DRAINING FIRST PATCH");
+//    while !patch_1.contexts.is_empty() {
+//        let flipped = patch_1.contexts.pop_front().unwrap().flip();
+//        for mut context in flipped.into_iter() {
+////            patch_1_offset += context.offset();
+////            trace!("PATCH 1 OFFSET: {}", patch_1_offset);
+////            context.shift(patch_2_offset);
+////            trace!("CONTEXT 1: {}", context);
+//            interdiff.contexts.push_back(context);
+//        }
+//    }
+//    trace!("DRAINING SECOND PATCH");
+//    while !patch_2.contexts.is_empty() {
+//        let reduced = patch_2.contexts.pop_front().unwrap().reduce();
+//        for mut context in reduced.into_iter() {
+////            patch_2_offset += context.offset();
+////            trace!("PATCH 2 OFFSET: {}", patch_2_offset);
+////            context.shift(patch_1_offset);
+////            trace!("CONTEXT 2: {}", context);
+//            interdiff.contexts.push_back(context);
+//        }
+//    }
+//
+//    trace!("{}", interdiff);
 
     Ok(())
 }
